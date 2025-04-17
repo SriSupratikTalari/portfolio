3a63c36fb966c714fba2800115edce2c13bf44bc
SriSupratikTalari <stalari@ucsd.edu>
2025-04-17 09:46:47 -0700

    step 3

--- global.js
+++ global.js
@@ -3,8 +3,33 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
-const navLinks = $$("nav a");
-let currentLink = navLinks.find(
-    (a) => a.host === location.host && a.pathname === location.pathname,
-  );
-currentLink?.classList.add("current");
+// const navLinks = $$("nav a");
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname,
+//   );
+// currentLink?.classList.add("current");
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    {url: 'contact/', title:'Contact'},
+    {url: 'resume/', title: 'Resume'},
+    { url: 'https://github.com/SriSupratikTalari', title: 'GitHub', external: true}
+  ];
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    nav.append(a);
+
+  }
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+? "/"                  // Local server
+: "/portfolio/";         // GitHub Pages repo name
+url = !url.startsWith('http') ? BASE_PATH + url : url;
+if (a.host === location.host && a.pathname === location.pathname) {
+    a.classList.add('current');
+  }
