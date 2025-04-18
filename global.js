a93f65e210c184ae3a7e9fb8ecd9b23344aaf9e7
SriSupratikTalari <stalari@ucsd.edu>
2025-04-18 11:50:48 -0700

    fixed the issue with pages

--- global.js
+++ global.js
@@ -17,19 +17,18 @@ let pages = [
   ];
 let nav = document.createElement('nav');
 document.body.prepend(nav);
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+? "/"                  // Local server
+: "/portfolio/"; 
 for (let p of pages) {
     let url = p.url;
+    url = !url.startsWith('http') ? BASE_PATH + url : url;
     let title = p.title;
     let a = document.createElement('a');
     a.href = url;
     a.textContent = title;
     nav.append(a);
-    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
   }
-const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
-? "/"                  // Local server
-: "/portfolio/";         // GitHub Pages repo name
-url = !url.startsWith('http') ? BASE_PATH + url : url;
 if (a.host === location.host && a.pathname === location.pathname) {
     a.classList.add('current');
   }
