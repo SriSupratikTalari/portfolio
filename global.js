c32ad37438f312db8423f2c4cbaf55385f63cb29
SriSupratikTalari <stalari@ucsd.edu>
2025-04-24 15:49:13 -0700

    push

--- global.js
+++ global.js
@@ -78,15 +78,18 @@ export async function fetchJSON(url) {
     console.error('Error fetching or parsing JSON data:', error);
   }
 }
-export function renderProjects(project, containerElement, headingLevel = 'h2') {
+export function renderProjects(projects, containerElement, headingLevel = 'h2') {
   containerElement.innerHTML = '';
-  const article = document.createElement('article');
-  article.innerHTML = `
-    <h3>${project.title}</h3>
-    <img src="${project.image}" alt="${project.title}">
-    <p>${project.description}</p>
-  `;
-  containerElement.appendChild(article);
+
+  for (const project of projects) {
+    const article = document.createElement('article');
+    article.innerHTML = `
+      <${headingLevel}>${project.title}</${headingLevel}>
+      <img src="${project.image}" alt="${project.title}">
+      <p>${project.description}</p>
+    `;
+    containerElement.appendChild(article);
+  }
 }
 // export async function fetchGitHubData(username) {
 //   return fetchJSON(`https://api.github.com/users/${username}`);
