78e3e4800f5a66182d3a136a14f69c32fc8a5647
SriSupratikTalari <stalari@ucsd.edu>
2025-04-25 11:26:30 -0700

    fixed all issues

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const projectsContainer = document.querySelector('.projects');
 console.log(projects)
 renderProjects(latestProjects, projectsContainer, 'h2');
 const githubData = await fetchGitHubData('SriSupratikTalari');
+console.log(githubData)
 const profileStats = document.querySelector('#profile-stats');
 if (profileStats) {
     profileStats.innerHTML = `
           <dl>
+            <h2>My GitHub Stats</h2>
             <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
             <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
             <dt>Followers:</dt><dd>${githubData.followers}</dd>
