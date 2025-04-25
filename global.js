998da91bdb984bff615f73f85266964ef013aeb5
SriSupratikTalari <stalari@ucsd.edu>
2025-04-24 17:12:57 -0700

    only a few things left

--- global.js
+++ global.js
@@ -91,6 +91,6 @@ export function renderProjects(projects, containerElement, headingLevel = 'h2') 
     containerElement.appendChild(article);
   }
 }
-// export async function fetchGitHubData(username) {
-//   return fetchJSON(`https://api.github.com/users/${username}`);
-// }
+export async function fetchGitHubData(username) {
+  return fetchJSON(`https://api.github.com/users/${username}`);
+}
