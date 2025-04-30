aa6783e9a835ced3ffce8db90e0894df258303b4
SriSupratikTalari <stalari@ucsd.edu>
2025-04-29 23:55:35 -0700

    lab5

--- projects/projects.js
+++ projects/projects.js
@@ -4,3 +4,49 @@ const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
 const title = document.querySelector('.projects-title');
 title.textContent = `${projects.length} Projects`;
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+let arc = arcGenerator({
+    startAngle: 0,
+    endAngle: 2 * Math.PI,
+  });
+d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
+let data = [
+    { value: 1, label: 'apples' },
+    { value: 2, label: 'oranges' },
+    { value: 3, label: 'mangos' },
+    { value: 4, label: 'pears' },
+    { value: 5, label: 'limes' },
+    { value: 5, label: 'cherries' },
+  ];
+let sliceGenerator = d3.pie().value((d) => d.value);
+let total = 0;
+for (let d of data) {
+  total += d;
+}
+let angle = 0;
+let arcData = sliceGenerator(data);
+
+for (let d of data) {
+  let endAngle = angle + (d / total) * 2 * Math.PI;
+  arcData.push({ startAngle: angle, endAngle });
+  angle = endAngle;
+}
+let arcs = arcData.map((d) => arcGenerator(d));
+arcs.forEach((arc) => {
+    // TODO, fill in step for appending path to svg using D3
+});
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
+arcs.forEach((arc, idx) => {
+    d3.select('svg')
+      .append('path')
+      .attr('d', arc)
+      .attr(colors) // Fill in the attribute for fill color via indexing the colors variable
+})
+let legend = d3.select('.legend');
+data.forEach((d, idx) => {
+  legend
+    .append('li')
+    .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
+    .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
+});
