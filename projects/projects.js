47ae818bd056bdb5a8310e0233ece177ecbe17d7
SriSupratikTalari <stalari@ucsd.edu>
2025-05-01 12:36:58 -0700

    almost completed step 4

--- projects/projects.js
+++ projects/projects.js
@@ -11,14 +11,9 @@ let arc = arcGenerator({
     endAngle: 2 * Math.PI,
   });
 d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
-let data = [
-    { value: 1, label: 'apples' },
-    { value: 2, label: 'oranges' },
-    { value: 3, label: 'mangos' },
-    { value: 4, label: 'pears' },
-    { value: 5, label: 'limes' },
-    { value: 5, label: 'cherries' },
-  ];
+let data = rolledData.map(([year, count]) => {
+  return { value: count, label: year };
+});
 let sliceGenerator = d3.pie().value((d) => d.value);
 let total = 0;
 for (let d of data) {
@@ -49,4 +44,23 @@ data.forEach((d, idx) => {
     .append('li')
     .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
     .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
+});
+let rolledData = d3.rollups(
+  projects,
+  (v) => v.length,
+  (d) => d.year,
+);
+let query = '';
+
+let searchInput = document.querySelector('.searchBar');
+searchInput.addEventListener('change', (event) => {
+  // update query value
+  query = event.target.value;
+  // filter projects
+  let filteredProjects = projects.filter((project) => {
+    let values = Object.values(project).join('\n').toLowerCase();
+    return values.includes(query.toLowerCase());
+  });
+  // render filtered projects
+  renderProjects(filteredProjects, projectsContainer, 'h2');
 });
