1e6c96f09f66bbc5e9cb8ac1de4913c18e4c4d99
SriSupratikTalari <stalari@ucsd.edu>
2025-05-02 13:11:05 -0700

    lab 5 completed

--- projects/projects.js
+++ projects/projects.js
@@ -63,8 +63,9 @@ import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 //   // render filtered projects
 //   renderProjects(filteredProjects, projectsContainer, 'h2');
 // });
+let selectedIndex = -1;
+
 function renderPieChart(projectsGiven) {
-  // Clean up old SVG paths and legend items
   let newSVG = d3.select('#projects-pie-plot');
   newSVG.selectAll('path').remove();
 
@@ -75,35 +76,65 @@ function renderPieChart(projectsGiven) {
   let newRolledData = d3.rollups(
     projectsGiven,
     (v) => v.length,
-    (d) => d.year,
+    (d) => d.year
   );
 
-  // Format it into data D3 can use
   let newData = newRolledData.map(([year, count]) => ({
     value: count,
     label: year
   }));
 
-  // Generate new pie chart
   let colors = d3.scaleOrdinal(d3.schemeTableau10);
   let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
   let sliceGenerator = d3.pie().value(d => d.value);
   let arcData = sliceGenerator(newData);
 
+  // Draw pie chart paths
   arcData.forEach((d, i) => {
     newSVG
       .append('path')
       .attr('d', arcGenerator(d))
-      .attr('fill', colors(i));
+      .attr('fill', colors(i))
+      .attr('class', i === selectedIndex ? 'selected' : '')
+      .on('click', () => {
+        selectedIndex = selectedIndex === i ? -1 : i;
+
+        if (selectedIndex === -1) {
+          renderProjects(projects, projectsContainer, 'h2');
+        } else {
+          const selectedYear = newData[selectedIndex].label;
+          const filteredProjects = projects.filter(p => p.year === selectedYear);
+          renderProjects(filteredProjects, projectsContainer, 'h2');
+        }
+        newSVG.selectAll('path')
+          .attr('class', (_, idx) => idx === selectedIndex ? 'selected' : '');
+
+        // Update all legend items
+        newLegend.selectAll('li')
+          .attr('class', (_, idx) =>
+            `legend-item${idx === selectedIndex ? ' selected' : ''}`
+          );
+      });
   });
 
-  // Update legend
+  // Draw legend
   newData.forEach((d, idx) => {
     newLegend
       .append('li')
       .attr('style', `--color:${colors(idx)}`)
-      .attr('class', 'legend-item')
-      .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);
+      .attr('class', `legend-item${idx === selectedIndex ? ' selected' : ''}`)
+      .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`)
+      .on('click', () => {
+        selectedIndex = selectedIndex === idx ? -1 : idx;
+
+        newSVG.selectAll('path')
+          .attr('class', (_, i) => i === selectedIndex ? 'selected' : '');
+
+        newLegend.selectAll('li')
+          .attr('class', (_, i) =>
+            `legend-item${i === selectedIndex ? ' selected' : ''}`
+          );
+      });
   });
 }
 
